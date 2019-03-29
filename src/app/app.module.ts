481eea26a79bf9e9099ec45ba0aab1b6321677c8
Oscar Espinel <oscaresp.194@gmail.com>
2019-03-28 20:30:30 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VerAvanceRedComponent } from './ver-avance-red/ver-avance-red.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VerAvanceRedComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ver-avance-red/ver-avance-red.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-avance-red',
+  templateUrl: './ver-avance-red.component.html',
+  styleUrls: ['./ver-avance-red.component.css']
+})
+export class VerAvanceRedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
