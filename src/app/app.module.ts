29813fe926a555ebb9d8a4f94a8006d59a0d28e9
se-mende <se-mende@uniandes.edu.co>
2019-03-28 20:26:56 -0500

    - Agregada lógica para conectar con Backend.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SubirREDComponent } from './subir-red/subir-red.component';
@@ -10,7 +11,9 @@ import { SubirREDComponent } from './subir-red/subir-red.component';
     SubirREDComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/subir-red/subir-red.component.ts
+++ src/app/subir-red/subir-red.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import {DatePipe} from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-subir-red',
   templateUrl: './subir-red.component.html',
   styleUrls: ['./subir-red.component.css']
 })
-export class SubirREDComponent implements OnInit {
+export class SubirREDComponent {
 
-  constructor() { }
+  form: FormGroup;
 
-  ngOnInit() {
+  @ViewChild('fileInput') fileInput: ElementRef;
+
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      comments: ['', Validators.required],
+      REDFile: null
+    });
+  }
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.form.get('REDFile').setValue(file);
+    }
+  }
+
+  private prepareSave(): any {
+    const input = new FormData();
+    input.append('comments', this.form.get('comments').value);
+    input.append('REDFile', this.form.get('REDFile').value);
+    return input;
+  }
+
+  onSubmit() {
+    const formModel = this.prepareSave();
+    // http post API URL goes here instead of the timer...
+    setTimeout(() => {
+      console.log(formModel.get('comments'));
+      console.log(formModel.get('REDFile'));
+      alert('done!');
+    }, 1000);
   }
 }
 
