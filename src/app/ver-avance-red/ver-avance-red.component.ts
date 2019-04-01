b31777e37a7eb065abd905cd6e85d07bf638ab27
Oscar Espinel <oscaresp.194@gmail.com>
2019-04-01 18:40:19 -0500

    Update url-constant

--- src/app/ver-avance-red/ver-avance-red.component.ts
+++ src/app/ver-avance-red/ver-avance-red.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { RED } from './Models/red';
+import { ActivatedRoute } from '@angular/router';
+import { AdvanceRedRestClientService } from '../services/advance-red-rest-client.service';
 
 @Component({
   selector: 'app-ver-avance-red',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerAvanceRedComponent implements OnInit {
 
-  constructor() { }
+  public red: RED;
+  subred: RED;
+  public advanceRedForm: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    
+    private route: ActivatedRoute,
+    private advanceRedRestClientService: AdvanceRedRestClientService) {
+      this.route.params.subscribe();
+   }
+
+  ngOnInit(): void {
+    this.loadForm();
+    this.getAdvanceRed();
+  }
+
+  loadForm(): void {
+    this.advanceRedForm = new FormGroup({
+      nombre: new FormControl(""),
+      getProyecto: new FormControl(""),
+      getFase: new FormControl(""),
+    });
+  }
+
+  public getAdvanceRed(): void {
+    this.advanceRedRestClientService
+      .getAdvanceRed()
+      .subscribe(response => {
+        this.red = response[0];
+        this.subred = response;
+      });
   }
 
 }
