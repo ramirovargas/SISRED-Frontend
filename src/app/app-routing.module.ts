0628bf21ec6b74c68420c51547d2892f7cb69346
cfsepulveda <42418118+cfsepulveda@users.noreply.github.com>
2019-04-03 19:53:00 -0500

    Equipo1 sprint1 (#6)
    * Configuracion inicial
    
    * in dev....
    
    * in dev
    
    * Primer commit
    
    * in dev
    
    * Avance
    
    * FuncionalidadCompleta
    
    * added bootstrap dependency
    
    * feature resource detail view and add
    
    * Plugin documentacion
    
    * added unit test y fix template
    
    * fix upadte
    
    * added notify js
    
    * title
    
    * Agregar Recurso RED finalizado sin unittest
    
    * Correcciones para aprovación de historia de usuario
    
    * Finish sisred2_ohep
    
    * Documentation sisred2 ver-avance-red
    
    * Update url-constant
    
    * Update html ver-avance-red
    
    * dependencia
    
    * ajuste deploy
    
    * se ajusta estructura del proyecto
    
    * remove
    
    * remove

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,46 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AddRedComponent } from './components/add-red/add-red.component';
-import { RedAsignadosComponent } from './components/red/asignaciones/red-asignados.component';
-import { HttpClientModule } from '@angular/common/http';
-import {DetalleREDComponent} from './components/detalle-red/detalle-red.component';
-import {ProyectosRedComponent} from './components/proyectos-red/proyectos-red.component';
-import { CommonModule } from '@angular/common';
-import { RedsRelacionadosComponent } from './components/reds-relacionados/reds-relacionados.component';
-import { RedRecursosComponent } from './components/red-recursos/red-recursos.component';
-import { RedRecursosDetalleComponent } from './components/red-recursos-detalle/red-recursos-detalle.component';
-
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AddRedComponent } from "./components/add-red/add-red.component";
+import { AgregarRecursoComponent } from "./components/agregar-recurso/agregar-recurso.component";
+import { DetalleRecursoComponent } from "./components/detalle-recurso/detalle-recurso.component";
+import { DetalleREDComponent } from "./components/detalle-red/detalle-red.component";
+import { ProyectosRedComponent } from "./components/proyectos-red/proyectos-red.component";
+import { RedRecursosDetalleComponent } from "./components/red-recursos-detalle/red-recursos-detalle.component";
+import { RedRecursosComponent } from "./components/red-recursos/red-recursos.component";
+import { RedAsignadosComponent } from "./components/red/asignaciones/red-asignados.component";
+import { RedsRelacionadosComponent } from "./components/reds-relacionados/reds-relacionados.component";
+import { VerAvanceRedComponent } from "./components/ver-avance-red/ver-avance-red.component";
 
 const routes: Routes = [
-  {path: '', component: RedAsignadosComponent},
-  {path: 'red/:idRed/proyecto/agregar', component: AddRedComponent},
-  {path: 'red/asignados/:idUsuario', component: RedAsignadosComponent},
-  {path: 'red/:idRed/detalle', component: DetalleREDComponent},
-  {path: 'red/:idRed/proyectos', component: ProyectosRedComponent},
+  { path: "", component: RedAsignadosComponent },
+  { path: "red/:idRed/proyecto/agregar", component: AddRedComponent },
+  { path: "red/asignados/:idUsuario", component: RedAsignadosComponent },
+  { path: "red/:idRed/detalle", component: DetalleREDComponent },
+  { path: "red/:idRed/proyectos", component: ProyectosRedComponent },
   {
-    path: 'redrel/:idProyectoC',
+    path: "redrel/:idProyectoC",
     component: RedsRelacionadosComponent
   },
   {
-    path: 'red/:id/recursos',
+    path: "red/:id/recursos",
     component: RedRecursosComponent
   },
   {
-    path: 'red/:id/recursos/:idrecurso',
+    path: "red/:id/recursos/:idrecurso",
     component: RedRecursosDetalleComponent
+  },
+  {
+    path: "recurso/agregar-recurso",
+    component: AgregarRecursoComponent
+  },
+  {
+    path: "recurso/detalle/:id",
+    component: DetalleRecursoComponent
+  },
+  {
+    path: "ver/avance-red/:id",
+    component: VerAvanceRedComponent
   }
 ];
 
@@ -35,4 +48,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes), HttpClientModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { NgSpinKitModule } from 'ng-spin-kit';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgbAlertModule, NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgSpinKitModule } from "ng-spin-kit";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AddRedComponent } from "./components/add-red/add-red.component";
+import { AgregarRecursoComponent } from "./components/agregar-recurso/agregar-recurso.component";
+import { DetalleRecursoComponent } from "./components/detalle-recurso/detalle-recurso.component";
+import { DetalleREDComponent } from "./components/detalle-red/detalle-red.component";
+import { ProyectosRedComponent } from "./components/proyectos-red/proyectos-red.component";
+import { RedRecursosDetalleComponent } from "./components/red-recursos-detalle/red-recursos-detalle.component";
+import { RedRecursosComponent } from "./components/red-recursos/red-recursos.component";
+import { RedAsignadosComponent } from "./components/red/asignaciones/red-asignados.component";
+import { RedsRelacionadosComponent } from "./components/reds-relacionados/reds-relacionados.component";
+import { VerAvanceRedComponent } from "./components/ver-avance-red/ver-avance-red.component";
+import { AgregarRecursoClientService } from './services/recurso/agregar/agregar-recurso-client.service';
+import { ResourceDetailsRestClientService } from './services/recurso/detalle/resource-details-rest-client.service';
 
-import { AppComponent } from './app.component';
-import { AddRedComponent } from './components/add-red/add-red.component';
-import { AppRoutingModule } from './app-routing.module';
-import { RedAsignadosComponent } from './components/red/asignaciones/red-asignados.component';
-import { DetalleREDComponent } from './components/detalle-red/detalle-red.component';
-import { ProyectosRedComponent } from './components/proyectos-red/proyectos-red.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RedsRelacionadosComponent } from './components/reds-relacionados/reds-relacionados.component';
-import { RedRecursosDetalleComponent } from './components/red-recursos-detalle/red-recursos-detalle.component';
-import { RedRecursosComponent } from './components/red-recursos/red-recursos.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +32,10 @@ import { RedRecursosComponent } from './components/red-recursos/red-recursos.com
     AppComponent,
     RedsRelacionadosComponent,
     RedRecursosDetalleComponent,
-    RedRecursosComponent
+    RedRecursosComponent,
+    DetalleRecursoComponent,
+    VerAvanceRedComponent,
+    AgregarRecursoComponent
   ],
   imports: [
     AppRoutingModule,
@@ -35,9 +45,12 @@ import { RedRecursosComponent } from './components/red-recursos/red-recursos.com
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    NgSpinKitModule
+    NgSpinKitModule,
+    NgbAlertModule,
+    NgbPaginationModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AgregarRecursoClientService, ResourceDetailsRestClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
