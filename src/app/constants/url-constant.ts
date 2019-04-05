93474ef0018e584b031be4096972dbc311e18787
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2019-04-04 20:42:39 -0500

    cambio de end points

--- src/app/constants/url-constant.ts
+++ src/app/constants/url-constant.ts
@@ -1,6 +1,6 @@
 export class UrlConstant {
-    static readonly SERVER_URL = "https://grupo1-sisred.herokuapp.com";
-    static readonly RESOURCE_DETAILS_ENDPOINT = "/recurso/recurso_get/";
-    static readonly RESOURCE_DETAILS_UPDATE_ENDPOINT = "/recurso/recurso_put/";
-    static readonly ADVANCE_RED_ENDPOINT = "/recurso/fase_byid/";
+    static readonly SERVER_URL = "http://sisredbackend.herokuapp.com";
+    static readonly RESOURCE_DETAILS_ENDPOINT = "/api/recurso/recurso_get/";
+    static readonly RESOURCE_DETAILS_UPDATE_ENDPOINT = "/api/recurso/recurso_put/";
+    static readonly ADVANCE_RED_ENDPOINT = "/api/recurso/fase_byid/";
 }
