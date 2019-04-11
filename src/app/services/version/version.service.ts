da523fc819f12d1de78839a6c672b76eec8182de
se-mende <se-mende@uniandes.edu.co>
2019-04-11 16:22:20 -0500

    - Versión vistas y servicios funcional integrada con el backend.

--- src/app/services/version/version.service.ts
+++ src/app/services/version/version.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Version } from './version.model';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
 
 
@@ -17,13 +17,13 @@ export class VersionService {
     // Metodo que invoca al servicio que obtiene las versiones del RED
   getVersiones(idRed: number): Observable<Version[]> {
     const apiUrlFinal = this.API_URL.replace('{id}', idRed.toString());
-
-    this.httpClient.get(apiUrlFinal).subscribe((data: Array<any>) => {
-      data.forEach(dataItem => {
+    this.versiones = [];
+    this.httpClient.get(apiUrlFinal).subscribe((data: any) => {
+      data.context.forEach(dataItem => {
         const version = new Version();
         version.numero = dataItem.numero;
         version.fechaCreacion = dataItem.fecha_creacion;
-        version.creadoPor = dataItem.creadoPor;
+        version.creadoPor = dataItem.creado_por.usuario.username;
         version.esFinal = dataItem.es_final;
         version.imagen = dataItem.imagen;
         this.versiones.push(version);
