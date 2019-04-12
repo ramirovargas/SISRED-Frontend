bb7e1d3654687205659729a8974a27bc82928fee
Juan Camilo Piza Jerez <juan.piza@globant.com>
2019-04-12 10:38:08 -0500

    Arreglos sobre servicio de marcar como final

--- src/app/services/version/version.service.ts
+++ src/app/services/version/version.service.ts
@@ -3,6 +3,7 @@ import { Observable, of } from 'rxjs';
 import { Version } from './version.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({
@@ -15,25 +16,27 @@ export class VersionService {
 
   constructor(private httpClient: HttpClient) { }
 
-    // Metodo que invoca al servicio que obtiene las versiones del RED
+  // Metodo que invoca al servicio que obtiene las versiones del RED
   getVersiones(idRed: number): Observable<Version[]> {
     const apiUrlFinal = this.API_URL.replace('{id}', idRed.toString());
     this.versiones = [];
     return this.httpClient.get(apiUrlFinal).pipe(map((data: Array<any>) => {
-        data.forEach(dataItem => {
-          const version = new Version();
-          version.numero = dataItem.numero;
-          version.fechaCreacion = dataItem.fecha_creacion;
-          version.creadoPor = dataItem.creadoPor;
-          version.esFinal = dataItem.es_final;
-          version.imagen = dataItem.imagen;
-          return version;
-        });
-      }));
+
+      return data.map(dataItem => {
+        const version = new Version();
+        version.numero = dataItem.numero;
+        version.fechaCreacion = dataItem.fecha_creacion;
+        version.creadoPor = dataItem.creadoPor;
+        version.esFinal = dataItem.es_final;
+        version.imagen = dataItem.imagen;
+        return version;
+      });
+
+    }));
   }
-  
+
   markAsFinal(idRed: number): Observable<Version> {
     const apiUrlMarcar = this.MARCAR_VERSION_URL.replace('{id}', idRed.toString());
-	this.httpClient.post(apiUrlMarcar)
+    return this.httpClient.post<Version>(apiUrlMarcar, "");
   }
 }
