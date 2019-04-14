0b4b4c2ce56d00ddec1adf5af7ea793c2550fe94
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-04-14 13:36:36 -0500

    Dashboard segmentado en componentes

---
+++ src/app/components/dashboard/tab-panes/tab-panes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab-panes',
+  templateUrl: './tab-panes.component.html',
+  styleUrls: ['./tab-panes.component.css']
+})
+export class TabPanesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
