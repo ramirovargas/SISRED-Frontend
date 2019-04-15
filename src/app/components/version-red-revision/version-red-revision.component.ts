f62c67ff6e9582a723e4e4c8861edd04df73363f
Rafael <raf_berm2@yahoo.com>
2019-04-14 22:20:11 -0500

    Added the component for the title and version of the red and the search table fot the list of resources

--- src/app/components/version-red-revision/version-red-revision.component.ts
+++ src/app/components/version-red-revision/version-red-revision.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+declare function setup(): any;
+
 @Component({
   selector: 'app-version-red-revision',
   templateUrl: './version-red-revision.component.html',
   styleUrls: ['./version-red-revision.component.css']
 })
+
 export class VersionRedRevisionComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    setup();
   }
 
 }
