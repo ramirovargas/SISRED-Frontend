7e769e472bfea04f7154a54ad4ec341fcc265d10
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2019-04-18 11:54:36 -0500

    Iniciar sesión implementado

---
+++ src/app/guards/autenticacion/autenticacion.guard.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AutenticacionService } from 'src/app/services/autenticacion/autenticacion.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionGuard implements CanActivate {
+  constructor(
+    private autenticacionService: AutenticacionService,
+    private router: Router
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const autenticado = this.autenticacionService.autenticado();
+
+    if (!autenticado) {
+      this.router.navigate(['']);
+    }
+
+    return true;
+  }
+}
---
+++ src/app/services/autenticacion/autenticacion.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import * as crypto from 'crypto-js';
+import { DatosUsuario } from 'src/app/models/datos-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+  constructor(private http: HttpClient) {}
+
+  public autenti = false;
+
+  autenticado(): boolean {
+    console.log(localStorage.getItem('token'));
+    return localStorage.getItem('token') !== null;
+  }
+
+  obtenerToken() {
+    const token = localStorage.getItem('token');
+    if (token === null) {
+      this.borrarDatos();
+      window.location.reload();
+    }
+    return token;
+  }
+
+  obtenerDatosUsuario(): DatosUsuario {
+    let datosUsuario = null;
+    const token = this.obtenerToken();
+    const usuarioTexto = localStorage.getItem('SISRED');
+    if (usuarioTexto === null) {
+      this.borrarDatos();
+      window.location.reload();
+    } else {
+      try {
+        const usuario = JSON.parse(
+          crypto.AES.decrypt(usuarioTexto, token).toString(crypto.enc.Utf8)
+        );
+
+        datosUsuario = {
+          nombre: usuario.firstName,
+          apellido: usuario.lastName,
+          idConectate: usuario.idConectate,
+          email: usuario.email,
+          numeroIdentificacion: usuario.numeroIdentificacion
+        };
+      } catch (err) {
+        this.borrarDatos();
+        window.location.reload();
+      }
+    }
+    return datosUsuario;
+  }
+
+  login(username, password) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json'
+        })
+      };
+      this.http
+        .post(
+          environment.apiUrl + 'login/',
+          {
+            username,
+            password
+          },
+          options
+        )
+        .subscribe(
+          data => {
+            resolve(data);
+          },
+          err => {
+            reject(err.error.error);
+          }
+        );
+    });
+  }
+
+  guardarDatos(data, email) {
+    const token = data.token;
+    localStorage.setItem('token', token);
+    delete data.token;
+    data.email = email;
+
+    console.log(data);
+
+    localStorage.setItem(
+      'SISRED',
+      crypto.AES.encrypt(JSON.stringify(data), token).toString()
+    );
+  }
+
+  borrarDatos() {
+    localStorage.clear();
+  }
+}
