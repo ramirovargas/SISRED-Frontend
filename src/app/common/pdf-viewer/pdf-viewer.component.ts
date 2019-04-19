f58ab0598221419535f7a6a2f9e4599e38a9e8d2
cfsepulveda <42418118+cfsepulveda@users.noreply.github.com>
2019-04-19 10:54:21 -0500

    pdf viewer

---
+++ src/app/common/pdf-viewer/pdf-viewer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-viewer',
+  templateUrl: './pdf-viewer.component.html',
+  styleUrls: ['./pdf-viewer.component.css']
+})
+export class PdfViewerComponent implements OnInit {
+
+  constructor() { }
+
+  @Input()
+  pdfSrc: string
+
+  ngOnInit() {
+  }
+
+}
