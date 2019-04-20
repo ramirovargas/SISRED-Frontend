960eb2bf92b43bf29553ed1714a4425b87382500
Daury <daurylecca16@gmail.com>
2019-04-20 12:59:15 -0500

    Comentarios versión video

---
+++ src/app/services/recurso/comments-version-video.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentsVersionVideoService {
+  API_URL = environment.apiUrl + 'comentarios/video/';
+  annotations: any;
+  constructor(private httpClient: HttpClient) { }
+
+  // Metodo que invoca al servcio que obitiene los comentarios del video
+  getCommentsVersionVideo(idRecurso: number): Array<any> {
+    this.httpClient.get(this.API_URL + idRecurso).subscribe((data: any) => {
+      this.annotations = data;
+    });
+    return this.annotations;
+  }
+}
