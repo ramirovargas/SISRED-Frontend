64cf7a5c24ad557b7e795bb9d7990b2f6dac82b9
stillirrom <irrom@hotmail.com>
2019-04-26 21:15:55 -0500

    agregando urls

--- src/app/services/comentario/comentar-imagen.service.ts
+++ src/app/services/comentario/comentar-imagen.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ComentarImagenService {
 
-  constructor() { }
+  COMENTARIO_EXISTENTE_URL = environment.apiUrl + 'versiones/{id_v}/recursos/{id_r}/comentarios/';
+  COMENTARIO_NUEVO_URL = environment.apiUrl + 'versiones/{id_v}/recursos/{id_r}/comentariosnuevos/';
+
+  constructor(private httpClient: HttpClient) { }
 }
