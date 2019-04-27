768c4c4b74e5986d9511fb3b8290133993197bed
Jose Laverde <jf.laverde@uniandes.edu.co>
2019-04-27 00:37:24 -0500

    se implementan las vistas con el back de buscar recurso

--- src/app/components/buscar-recurso/buscar-recurso.component.ts
+++ src/app/components/buscar-recurso/buscar-recurso.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgxSpinnerService} from 'ngx-spinner';
+import { Recurso } from '../../services/recurso/recurso';
+import { BuscarRecursoService } from '../../services/recurso/buscar-recurso.service';
 
 declare function setup(): any;
 
@@ -12,12 +14,15 @@ declare function setup(): any;
 export class BuscarRecursoComponent implements OnInit {
 
   public buscarRecursoForm: FormGroup;
-  private recursos = [];
+
+  recursos: Recurso[] = [];
+  recursosSinRepetir: Recurso[] = [];
   pagina: number = 1;
   paginaSize: number = 5;
+  cargando: boolean = false;
 
-  constructor() {
-  }
+
+  constructor(private buscarRecursoService: BuscarRecursoService, private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
     setup();
@@ -27,6 +32,60 @@ export class BuscarRecursoComponent implements OnInit {
       fechaHasta: new FormControl(),
       tag: new FormControl(),
     });
+  }
+
+  buscarRecurso() {
+    this.recursos = [{
+      id: '1',
+      nombre: 'mi red',
+      archivo: 'mi red',
+      thumbnail: 'esta es una descripción',
+      fecha_creacion: '2019-02-12',
+      fecha_ultima_modificacion: '2019-04-22',
+      tipo: 'string',
+      descripcion: '1',
+      metadata: '3',
+      autor: 'string',
+      usuario_ultima_modificacion: '1',
+    },];
+    console.log({
+      fecha_creacion: this.buscarRecursoForm.get('fechaDesde').value,
+      nombre: this.buscarRecursoForm.get('nombre').value,
+    });
+    this.cargando = true;
+    this.spinner.show()
+    this.buscarRecursoService.buscarRecursos(this.buscarRecursoForm.get('nombre').value, this.formatFecha(this.buscarRecursoForm.get('fechaDesde').value),
+      this.formatFecha(this.buscarRecursoForm.get('fechaHasta').value), this.buscarRecursoForm.get('tag').value)
+      .then(recursos => {
+        console.log(recursos);
+        this.spinner.hide()
+        this.recursos = recursos;
+        this.eliminarRepetidos();
+      })
+      .catch(err => {
+        this.spinner.hide();
+        console.log(err);
+      });
+  }
+
+  formatFecha(fecha) {
+    let res = null;
+    if (fecha !== null) {
+      let year = fecha.year;
+      let month = fecha.month<10 ? `0${fecha.month}` : fecha.month;
+      let day = fecha.day<10 ? `0${fecha.day}` : fecha.day;
+      res = `${year}-${month}-${day}`;
+    }
+    return res;
+  }
+
+  eliminarRepetidos() {
+    this.recursosSinRepetir = [];
+    for(let recurso of this.recursos) {
+      if(this.recursosSinRepetir.filter(r => r.id === recurso.id).length === 0) {
+        this.recursosSinRepetir.push(recurso);
+      }
+    }
   }
 
 }
